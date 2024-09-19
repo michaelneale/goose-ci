ee272c2a1d3d49c6028ad4463ad09aeed1cceec1
Mic Neale <michael.neale@gmail.com>
2024-09-19 16:48:56 +1000

    trying more

--- .github/actions/goose-action/entrypoint.sh
+++ .github/actions/goose-action/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Write the profile content to the expected location
-mv /root/.goose/config/profiles.yaml /root/.goose/config/profile.yaml
 
 # Execute the command passed to the Docker container
 # Start the Goose session and run it in the background
@@ -9,10 +7,10 @@ goose version
 
 # Poll for success or failure file
 while true; do
-    if [ -f /root/.goose/config/success ]; then
+    if [ -f /app/success ]; then
         echo "Goose session succeeded"
         exit 0
-    elif [ -f /root/.goose/config/failure ]; then
+    elif [ -f /app/failure ]; then
         echo "Goose session failed"
         exit 1
     fi
