8ea77ed16d24306e4e1cc98bd08560adbffcd032
michaelneale <14976+michaelneale@users.noreply.github.com>
2024-09-23 07:43:10 +0000

    goose changes

---
+++ months_in_japanese.rb
@@ -0,0 +1,17 @@
+# Ruby code to print the months of the year in Japanese
+months = [
+  "一月", # January
+  "二月", # February
+  "三月", # March
+  "四月", # April
+  "五月", # May
+  "六月", # June
+  "七月", # July
+  "八月", # August
+  "九月", # September
+  "十月", # October
+  "十一月", # November
+  "十二月" # December
+]
+
+months.each { |month| puts month }
